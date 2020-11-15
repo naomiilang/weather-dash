c5685cf6b4e1c49caa8cbe6b14cc22ab6f26e952
Naomi <naomilang1212@gmail.com>
2020-11-14 22:03:09 -0800

    functional todays info

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var todaysCity = document.querySelector("#todaysCity");
 var tempEl = document.querySelector("#tempEl");
 var humidtyEl = document.querySelector("#humidityEl");
+var windEl = document.querySelector("#windEl");
+var uvEl = document.querySelector("uvEl");
+var cardDate = document.querySelector("#card1date");
+var cardTemp = document.querySelector("cardTemp");
+var cardHumidity = document.querySelector("#card1Humidity");
 
 var searchCity = function () {
     var searchInput = document.querySelector("#citySearch").value;
@@ -11,17 +17,36 @@ var searchCity = function () {
         searchInput +
         '&units=imperial' +
         '&appid=' +
-        apiKey 
+        apiKey
     )
-        .then(function (response) {
-            return response.json();
+        .then(function (Wresponse) {
+            return Wresponse.json();
         })
-        .then(function (response) {
-            console.log(response);
-            var temp = response.main.temp;
-            tempEl.append(temp);
-            var humidity = response.main.humidity;
-            humidityEl.append(humidity);
+        .then(function (Wresponse) {
+            console.log(Wresponse);
+            var temp = Wresponse.main.temp;
+            tempEl.append(temp + "°");
+            var humidity = Wresponse.main.humidity;
+            humidityEl.append(humidity + "%");
+            var city = Wresponse.name;
+            todaysCity.append(city);
+            var wind = Wresponse.wind.speed;
+            windEl.append(wind + " mph");
         })
 
+    fetch (
+       'https://api.openweathermap.org/data/2.5/forecast?q=' +
+       searchInput +
+       '&units=imperial' +
+       '&appid=' +
+       apiKey
+    )   
+        .then(function(Fresponse){
+            return Fresponse.json();
+        })
+        .then(function(Fresponse){
+            console.log(Fresponse);
+            var carddate = Fresponse.list[0].dt_txt;
+            cardDate.append(carddate);
+        })
 };
